08ee66877ad450f740beec0b819e572cf34dc33b
Amund <mailamund@gmail.com>
2025-04-28 18:57:03 +0200

    fix: loading error from some url

--- proxy.php
+++ proxy.php
@@ -1,40 +1,32 @@
-<?php //é
+<?php
 
 ob_start();
 ini_set( 'display_errors', 1 );
 
-$cache = 3600;
 $url = base64_decode( $_REQUEST['url'] );
+$content = curl_file_get_contents( $url );
+die( $content );
 
-$local = __DIR__.'/cache/'.md5( $url );
+function curl_file_get_contents($url) {
+	$headers = [
+		'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0',
+		'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+		'Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+		'Connection: keep-alive',
+		'Pragma: no-cache',
+		'Cache-Control: no-cache',
+	];
 
-if( !is_file( $local ) || filemtime( $local ) < ( time() - $cache ) ) {
-	$context = stream_context_create( array( 'http' => array(
-		'header'=>"Connection: close\r\n".
-			"User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0\n\r"
-	) ) );
-	$content = file_get_contents( $url, 0, $context );
-	//$content = convertToUtf8( $content );
-	//$content = file_get_contents( $url );
-	die( $content );
-	//$content = curl_file_get_contents( $url );
-	$content = preg_replace( '#<content:encoded><!\[CDATA\[.*?\]\]></content:encoded>#s', '', $content );
-	file_put_contents( $local, $content );
-}
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	$data = curl_exec($ch);
+	curl_close($ch);
 
-header( 'Content-Type: text/xml' );
-//header( 'Content-Length: '.filesize( $local ) );
-echo file_get_contents( $local );
-
-
-function curl_file_get_contents($URL) {
-	$c = curl_init();
-	curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($c, CURLOPT_URL, $URL);
-	$contents = curl_exec($c);
-	curl_close($c);
-
-	if ($contents) return $contents;
+	if ($data) return $data;
 		else return FALSE;
 }
 
@@ -42,7 +34,7 @@ function convertToUtf8( $text ) {
 	if( preg_match( '#<\?xml.+encoding=("|\')?([^\1]*)\1.*\?>#i', $text, $matches ) ) {
 		$charset = $matches[2];
 		if( $charset && ( strtoupper( $charset )=='ISO-8859-1' || strtoupper( $charset )=='ISO-8859-15' ) ) {
-			$text = utf8_encode( $text );
+			$text = mb_convert_encoding( $text, 'UTF-8', $charset );
 			$text = preg_replace('/<\?xml.+encoding=("|\')?([^\1]*)\1.*\?>/i', '<?xml version="1.0" encoding="UTF-8"?>', $text);
 		}
 	}
